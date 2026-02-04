7f306db1bd5c32c0b8ddb04a4abec0c9761965d9
Embitude Trainings <info@embitude.in>
2026-02-04 16:47:40 +0530

    Added the application for ioctl

---
+++ Apps/led_ioctl.h
@@ -0,0 +1,13 @@
+#ifndef _LED_IOCTL_H
+
+#define _LED_IOCTL_H
+
+#include <linux/ioctl.h>
+
+#define GPIO_SELECT_LED _IOW('l', 1, int)
+
+//TODO 2: Define the IOCTLS
+#define GPIO_SET_LED	_IOW('l', 2, int)
+#define GPIO_GET_LED	_IOR('l', 3, int)
+
+#endif /* _LED_IOCTL_H */
